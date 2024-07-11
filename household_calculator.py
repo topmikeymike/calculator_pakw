747f7fb903ccfbf5f937026a8ecc00677a0e2e57
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2024-07-11 17:31:09 +0800

    Add files via upload

---
+++ household_calculator.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd
+import uuid  # For generating unique IDs
+
+@st.cache_data
+def load_data(file):
+    return pd.read_csv(file)
+
+def collect_household_data(data, generate_household_id):
+    num_households = st.number_input('Enter number of households', min_value=1, value=1, step=1)
+    selected_options_list = []
+    total_pakw_hh = 0
+
+    # Select Negeri
+    selected_negeri = st.selectbox('NEGERI', ["Select"] + list(data['NEGERI'].unique()), key=f'NEGERI_{1}')
+
+    # Filter Daerah options based on selected Negeri
+    if selected_negeri != "Select":
+        daerah_options = list(data[data['NEGERI'] == selected_negeri]['DAERAH'].unique())
+        selected_daerah = st.selectbox('DAERAH', ["Select"] + daerah_options, key=f'DAERAH_{1}')
+    else:
+        selected_daerah = st.selectbox('DAERAH', ["Select"], key=f'DAERAH_{1}')
+
+    # Filter Strata options based on selected Daerah
+    if selected_daerah != "Select":
+        strata_options = list(data[data['DAERAH'] == selected_daerah]['STRATA'].unique())
+        selected_strata = st.radio('STRATA', strata_options, key=f'STRATA_{1}')
+    else:
+        selected_strata = st.radio('STRATA', ["Select"], key=f'STRATA_{1}')
+
+    for household_id in range(num_households):
+        household_uuid = generate_household_id()  # Generate unique household ID for the session
+
+        with st.expander(f'Household {household_id + 1}'):
+            # Create dropdown lists for categorical columns for individual member
+            umur_ksh = st.selectbox('UMUR_KSH', ["Select"] + list(data['UMUR_KSH'].unique()), key=f'UMUR_KSH_{household_id}')
+            jantina = st.radio('JANTINA', list(data['JANTINA'].unique()), key=f'JANTINA_{household_id}')
+
+            # Calculate total expenditure for the household
+            if (umur_ksh != "Select" and jantina != "Select" and selected_negeri != "Select" and 
+                selected_daerah != "Select" and selected_strata != "Select"):
+                
+                filtered_data = data[
+                    (data['UMUR_KSH'] == umur_ksh) & 
+                    (data['JANTINA'] == jantina) &
+                    (data['NEGERI'] == selected_negeri) &
+                    (data['DAERAH'] == selected_daerah) &
+                    (data['STRATA'] == selected_strata)
+                ]
+
+                if not filtered_data.empty:
+                    total_mean_p_rent = filtered_data['Mean(p_rent)'].sum()
+                    mean_p_rent = total_mean_p_rent / num_households
+                    adjusted_mean_p_rent = mean_p_rent * (num_households ** 0.4745)
+
+                    total_pakw = (
+                        filtered_data['Mean(TOTAL_PAKW_MAKANAN)'].values[0] +
+                        adjusted_mean_p_rent +
+                        filtered_data['Mean(p_lain2)'].values[0]
+                    )
+
+                    selected_options = {
+                        'HOUSEHOLD_ID': household_uuid,
+                        'UMUR_KSH': umur_ksh,
+                        'JANTINA': jantina,
+                        'NEGERI': selected_negeri,
+                        'DAERAH': selected_daerah,
+                        'STRATA': selected_strata,
+                        'TOTAL PAKW': total_pakw,
+                        'TOTAL_HH': num_households  # Add total number of households
+                    }
+
+                    selected_options_list.append(selected_options)
+                    total_pakw_hh += total_pakw  # Accumulate total PAKW HH
+
+    # Set TOTAL PAKW HH for each entry if there are multiple households
+    if num_households > 1:
+        for option in selected_options_list:
+            option['TOTAL PAKW HH'] = total_pakw_hh
+
+    return selected_options_list
---
+++ main.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import pandas as pd
+import uuid
+
+st.set_page_config(
+    page_title="PAKW Calculator Apps",
+    page_icon="🧊",
+    #layout="wide",
+    initial_sidebar_state="expanded",
+    #menu_items={
+    #    'Get Help': 'https://www.extremelycoolapp.com/help',
+    #    'Report a bug': "https://www.extremelycoolapp.com/bug",
+    #    'About': "# This is a header. This is an *extremely* cool app!"
+    #}
+)
+
+# Custom CSS for styling
+st.markdown(
+    """
+    <style>
+    html {
+        font-size: 18px;
+    }
+    .reportview-container .main .block-container {
+        max-width: 90%;
+        padding: 2rem;
+    }
+    .footer-text {
+        font-size: 12px;
+        color: transparent;
+    }
+    .footer-text:hover {
+        color: grey;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+from household_calculator import collect_household_data, load_data
+
+# Function to reset session state
+def reset_session_state():
+    for key in st.session_state.keys():
+        del st.session_state[key]
+
+# Function to generate a unique household ID for the session
+def generate_household_id():
+    if 'household_id' not in st.session_state:
+        st.session_state.household_id = uuid.uuid4().hex[:8]  # Generate a random 8-character hex string
+    return st.session_state.household_id
+
+# Header
+st.title("Household Expenditure Calculator")
+st.markdown("Perbelanjaan Asas Kehidupan Wajar | (PAKW)")
+st.markdown("This calculator calculates the total PAKW expenditure based on user inputs. ")
+
+# Load the data
+data = load_data('/Users/michael.igo/Documents/project/prototype-main/pakw_v1/pakw_calculator_referal.csv')
+
+# Collect household data
+selected_options_list = collect_household_data(data, generate_household_id)
+
+# Display total household expenditure
+if 'calculate_clicked' not in st.session_state:
+    st.session_state.calculate_clicked = False
+
+if st.button('Calculate Total Household Expenditure'):
+    st.session_state.calculate_clicked = True
+    if selected_options_list:
+        st.write('Total Household Expenditure:')
+        total_household_expenditure = sum(option['TOTAL PAKW'] for option in selected_options_list)
+        st.write(f'TOTAL PAKW for all households: {total_household_expenditure}')
+
+        # Display TOTAL PAKW HH for all households if applicable
+        if selected_options_list[0]['TOTAL_HH'] > 1:
+            st.write(f'TOTAL PAKW HH for all households: {selected_options_list[0]["TOTAL PAKW HH"]}')
+
+        # Save selected options to CSV
+        selected_options_df = pd.DataFrame(selected_options_list)
+
+        # Conditionally add TOTAL PAKW HH column if there are multiple households
+        if selected_options_list[0]['TOTAL_HH'] > 1:
+            selected_options_df['TOTAL PAKW HH'] = selected_options_list[0]['TOTAL PAKW HH']
+
+        # Save to CSV
+        try:
+            history = pd.read_csv('/Users/michael.igo/Documents/project/prototype-main/pakw_v1user_input_history.csv')
+            history = pd.concat([history, selected_options_df], ignore_index=True)
+            history.to_csv('/Users/michael.igo/Documents/project/prototype-main/pakw_v1/user_input_history.csv', index=False)
+        except FileNotFoundError:
+            selected_options_df.to_csv('/Users/michael.igo/Documents/project/prototype-main/pakw_v1/user_input_history.csv', index=False)
+
+        # Display selected options in a minimalist table
+        st.write('Selected Options:')
+        # Only select relevant columns for display
+        display_columns = ['HOUSEHOLD_ID', 'UMUR_KSH', 'JANTINA', 'NEGERI', 'DAERAH', 'STRATA', 'TOTAL PAKW']
+        if selected_options_list[0]['TOTAL_HH'] > 1:
+            display_columns.append('TOTAL PAKW HH')
+        display_columns.append('TOTAL_HH')
+        st.dataframe(selected_options_df[display_columns], width=1000)  # Set width to accommodate all columns
+
+if st.session_state.calculate_clicked:
+    if st.button('Recalculate'):
+        reset_session_state()
+        st.experimental_rerun()
+
+# Footer
+language_dict = {
+    'footer': '© 2024 Household Expenditure Calculator',
+    'footer2': 'BANK IN DULU MAT'
+}
+
+footer_html = f"""
+    <div style="text-align:center; padding: 10px;">
+        <p style="font-size: 12px; color: grey;">{language_dict['footer']}</p>
+        <p class="footer-text">{language_dict['footer2']}</p>
+        <p style="font-size: 12px; color: grey;">Ver.1.0</p>
+    </div>
+"""
+st.markdown(footer_html, unsafe_allow_html=True)
