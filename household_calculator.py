27d11b9dc4e89f49d323fd048dfd6e930d4bf71e
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2024-07-12 15:50:03 +0800

    Update household_calculator.py

--- household_calculator.py
+++ household_calculator.py
@@ -37,11 +37,11 @@ def collect_household_data(data, generate_household_id):
 
         with st.expander(f'Isi Rumah {household_id + 1}'):
             # Create dropdown lists for categorical columns for individual member
-            st.markdown("<h2 style='font-size:20px;' >Langkah 5 : Masukkan maklumat ahli isi rumah</h2>", unsafe_allow_html=True)
             umur_ksh = st.selectbox('UMUR_KSH', ["Pilih"] + list(data['UMUR_KSH'].unique()), key=f'UMUR_KSH_{household_id}')
             jantina = st.radio('JANTINA', list(data['JANTINA'].unique()), key=f'JANTINA_{household_id}')
 
             # Calculate total expenditure for the household
+            st.markdown("<h2 style='font-size:20px;' >Langkah 5 : Masukkan maklumat ahli isi rumah</h2>", unsafe_allow_html=True)
             if (umur_ksh != "Pilih" and jantina != "Pilih" and selected_negeri != "Pilih" and 
                 selected_daerah != "Pilih" and selected_strata != "Pilih"):
                 
