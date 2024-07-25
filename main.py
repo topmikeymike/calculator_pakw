4ab22c7b9b592689153286caa9064bb50265f916
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2024-07-25 12:41:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -305,12 +305,12 @@ language_dict = {
 }
 
 footer_html = f"""
-    <div style="text-align:center; padding: 10px;">
-        <br><br><br><br>
+    <div style="text-align:center; padding: 10px; position: fixed; bottom: 0; width: 100%; background-color: white;">
         <p style="font-size: 12px; color: grey;">{language_dict['footer']}</p>
         <!-- <p class="footer-text">{language_dict['footer2']}</p> -->
         <!-- <p style="font-size: 12px; color: grey;">Ver.1.1</p> -->
     </div>
 """
+
 st.markdown(footer_html, unsafe_allow_html=True)
 
