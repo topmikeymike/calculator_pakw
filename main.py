5fafd0cdd5626fab4eeb81199cfbb7763765cea3
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2024-07-25 12:08:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -180,7 +180,8 @@ st.markdown("""
                 <a href="#"><i class="fas fa-globe"></i></a> 
                 <a href="https://web.facebook.com/StatsMalaysia"><i class="fab fa-facebook-f"></i></a>
                 <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg></a> 
-                <a href="#"><i class="fa-brands fa-x-twitter"></i></a> 
+                <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
                 <a href="#"><i class="fab fa-instagram"></i></a> 
                 <a href="#"><i class="fab fa-youtube"></i></a> 
             </div>
