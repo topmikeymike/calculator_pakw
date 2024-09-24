1fccad612ae4d421f8826152f4ca9478b14f3d02
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2024-09-24 16:21:48 +0800

    Update household_calculator.py

--- household_calculator.py
+++ household_calculator.py
@@ -38,7 +38,7 @@ def collect_household_data(data, generate_household_id):
 
         # Loop over the number of households
         for household_id in range(num_households):
-        st.markdown(f"### Isi Rumah {household_id + 1}")
+        st.markdown(f"### Isi Rumah ")
 
         # Create a header row to mimic the table in the image
         cols = st.columns([2, 1, 1])  # Define the width of each column (UMUR/JANTINA, LELAKI, PEREMPUAN)
